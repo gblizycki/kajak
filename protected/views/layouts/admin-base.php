fe130289393ce88ce9728c672f58a9d1120de78a
Grzegorz Bliżycki <grzegorzblizycki@gmail.com>
2011-12-29 12:06:58 +0100

    Podstawowe layout'y zastosowane w panelu do zarządzania bazą danych

---
+++ protected/views/layouts/admin-base.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="language" content="en" />
+
+	<!-- blueprint CSS framework -->
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
+	<!--[if lt IE 8]>
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
+	<![endif]-->
+
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+</head>
+
+<body>
+
+<div class="container" id="page">
+
+	<div id="header">
+		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
+	</div><!-- header -->
+
+	<div id="mainmenu">
+		<?php $this->widget('ext.mbmenu.MbMenu',array(
+			'items'=>array(
+				array('label'=>'Route', 'url'=>array('route/admin'),'items'=>array(
+                                    array('label'=>'Admin', 'url'=>array('route/admin')),
+                                    array('label'=>'Create', 'url'=>array('route/create')),
+                                )),
+				array('label'=>'Place', 'url'=>array('place/admin'),'items'=>array(
+                                    array('label'=>'Admin', 'url'=>array('place/admin')),
+                                    array('label'=>'Create', 'url'=>array('place/create')),
+                                )),
+				array('label'=>'Area', 'url'=>array('area/admin'),'items'=>array(
+                                    array('label'=>'Admin', 'url'=>array('area/admin')),
+                                    array('label'=>'Create', 'url'=>array('area/create')),
+                                )),
+				array('label'=>'Category', 'items'=>array(
+                                    array('label'=>'Route', 'url'=>array('categoryroute/admin'),'items'=>array(
+                                        array('label'=>'Admin', 'url'=>array('categoryroute/admin')),
+                                        array('label'=>'Create', 'url'=>array('categoryroute/create')),
+                                    )),
+                                    array('label'=>'Place', 'url'=>array('categoryplace/admin'),'items'=>array(
+                                        array('label'=>'Admin', 'url'=>array('categoryplace/admin')),
+                                        array('label'=>'Create', 'url'=>array('categoryplace/create')),
+                                    )),
+                                    array('label'=>'Area', 'url'=>array('categoryarea/admin'),'items'=>array(
+                                        array('label'=>'Admin', 'url'=>array('categoryarea/admin')),
+                                        array('label'=>'Create', 'url'=>array('categoryarea/create')),
+                                    )),
+                                )),
+				array('label'=>'Data Source', 'url'=>array('datasource/admin'),'items'=>array(
+                                    array('label'=>'Admin', 'url'=>array('datasource/admin')),
+                                    array('label'=>'Create', 'url'=>array('datasource/create')),
+                                )),
+				array('label'=>'User', 'url'=>array('user/admin'),'items'=>array(
+                                    array('label'=>'Admin', 'url'=>array('user/admin')),
+                                    array('label'=>'Create', 'url'=>array('user/create')),
+                                )),
+				array('label'=>'Login', 'url'=>array('site/login'), 'visible'=>Yii::app()->user->isGuest),
+				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+			),
+		)); ?>
+	</div><!-- mainmenu -->
+
+	<?php $this->widget('zii.widgets.CBreadcrumbs', array(
+		'links'=>$this->breadcrumbs,
+	)); ?><!-- breadcrumbs -->
+
+	<?php echo $content; ?>
+
+	<div id="footer">
+	</div><!-- footer -->
+
+</div><!-- page -->
+
+</body>
+</html>
---
+++ protected/views/layouts/admin.php
@@ -0,0 +1,14 @@
+<?php $this->beginContent('/layouts/admin-base'); ?>
+<div class="container">
+	<div class="span-18">
+		<div id="content">
+			<?php echo $content; ?>
+		</div><!-- content -->
+	</div>
+	<div class="span-6 last">
+		<div id="sidebar">
+			<?php $this->widget('ext.sidemenu.sidemenu',array('items'=>$this->menu)); ?>
+		</div><!-- sidebar -->
+	</div>
+</div>
+<?php $this->endContent(); ?>

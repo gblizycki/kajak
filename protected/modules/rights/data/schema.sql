bb7edbe7faee83dcb28191f4332977ff3eef2012
Grzegorz Bliżycki <grzegorzblizycki@gmail.com>
2012-02-09 12:34:59 +0100

    Poprawa adresu serwera

--- protected/modules/rights/data/schema.sql
+++ protected/modules/rights/data/schema.sql
@@ -1,9 +1,9 @@
-drop table if exists AuthItem;
-drop table if exists AuthItemChild;
-drop table if exists AuthAssignment;
-drop table if exists Rights;
+drop table if exists authitem;
+drop table if exists authitemChild;
+drop table if exists authassignment;
+drop table if exists rights;
 
-create table AuthItem
+create table authitem
 (
    name varchar(64) not null,
    type integer not null,
@@ -13,30 +13,30 @@ create table AuthItem
    primary key (name)
 );
 
-create table AuthItemChild
+create table authitemchild
 (
    parent varchar(64) not null,
    child varchar(64) not null,
    primary key (parent,child),
-   foreign key (parent) references AuthItem (name) on delete cascade on update cascade,
-   foreign key (child) references AuthItem (name) on delete cascade on update cascade
+   foreign key (parent) references authitem (name) on delete cascade on update cascade,
+   foreign key (child) references authitem (name) on delete cascade on update cascade
 );
 
-create table AuthAssignment
+create table authassignment
 (
    itemname varchar(64) not null,
    userid varchar(64) not null,
    bizrule text,
    data text,
    primary key (itemname,userid),
-   foreign key (itemname) references AuthItem (name) on delete cascade on update cascade
+   foreign key (itemname) references authitem (name) on delete cascade on update cascade
 );
 
-create table Rights
+create table rights
 (
 	itemname varchar(64) not null,
 	type integer not null,
 	weight integer not null,
 	primary key (itemname),
-	foreign key (itemname) references AuthItem (name) on delete cascade on update cascade
+	foreign key (itemname) references authitem (name) on delete cascade on update cascade
 );
